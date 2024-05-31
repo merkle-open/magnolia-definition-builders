f59f43733b69f55127e67ed8c41586e475844760
eschleb <bjoern.eschle@merkle.com>
2024-05-31 08:28:43 +0200

    Implement And-/OrAvailabilityRule

---
+++ custom-definitions/src/main/java/com/merkle/oss/magnolia/definition/custom/availability/OrAvailabilityRule.java
@@ -0,0 +1,78 @@
+package com.merkle.oss.magnolia.definition.custom.availability;
+
+import info.magnolia.objectfactory.ComponentProvider;
+import info.magnolia.ui.api.availability.AvailabilityDefinition;
+import info.magnolia.ui.api.availability.AvailabilityRule;
+import info.magnolia.ui.api.availability.AvailabilityRuleDefinition;
+import info.magnolia.ui.api.availability.ConfiguredAvailabilityRuleDefinition;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.inject.Inject;
+
+import com.merkle.oss.magnolia.definition.builder.availability.AbstractAvailabilityRuleDefinitionBuilder;
+
+public class OrAvailabilityRule implements AvailabilityRule {
+	private final AvailabilityDefinition availabilityDefinition;
+	private final Definition ruleDefinition;
+	private final ComponentProvider componentProvider;
+
+	@Inject
+	public OrAvailabilityRule(
+			final AvailabilityDefinition availabilityDefinition,
+			final Definition ruleDefinition,
+			final ComponentProvider componentProvider
+	) {
+		this.availabilityDefinition = availabilityDefinition;
+		this.ruleDefinition = ruleDefinition;
+		this.componentProvider = componentProvider;
+	}
+
+	@Override
+	public boolean isAvailable(final Collection<?> items) {
+		return ruleDefinition.rules
+				.stream()
+				.map(definition -> componentProvider.newInstance(definition.getImplementationClass(), availabilityDefinition, definition))
+				.anyMatch(rule -> rule.isAvailable(items));
+	}
+
+	public static class Definition extends ConfiguredAvailabilityRuleDefinition {
+		private final Set<AvailabilityRuleDefinition> rules;
+
+		public Definition(final Set<AvailabilityRuleDefinition> rules) {
+			setImplementationClass(OrAvailabilityRule.class);
+			this.rules = rules;
+		}
+
+		public static class Builder extends AbstractAvailabilityRuleDefinitionBuilder<Definition, Definition.Builder> {
+			private Set<AvailabilityRuleDefinition> rules = new HashSet<>();
+
+			public Builder() {}
+			public Builder(final Definition definition) {
+				super(definition);
+			}
+
+			public Definition.Builder rule(final AvailabilityRuleDefinition rule) {
+				return rules(Stream.concat(
+						Stream.ofNullable(rules).flatMap(Collection::stream),
+						Stream.of(rule)
+				).collect(Collectors.toSet()));
+			}
+
+			public Definition.Builder rules(final Set<AvailabilityRuleDefinition> rules) {
+				this.rules = rules;
+				return self();
+			}
+
+			public Definition build() {
+				final Definition definition = new Definition(rules);
+				populate(definition);
+				return definition;
+			}
+		}
+	}
+}
