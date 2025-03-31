e69782f96277ca8fd5c7bdc501f2549b22dc9cf8
eschleb <bjoern.eschle@merkle.com>
2025-03-31 12:30:42 +0200

    Add viewType to OpenDetailSubappActionDefinitionBuilder

--- magnolia-builders/src/main/java/com/merkle/oss/magnolia/definition/builder/contentapp/action/OpenDetailSubappActionDefinitionBuilder.java
+++ magnolia-builders/src/main/java/com/merkle/oss/magnolia/definition/builder/contentapp/action/OpenDetailSubappActionDefinitionBuilder.java
@@ -1,14 +1,26 @@
 package com.merkle.oss.magnolia.definition.builder.contentapp.action;
 
 import info.magnolia.ui.contentapp.action.OpenDetailSubappActionDefinition;
+import info.magnolia.ui.contentapp.detail.ContentDetailSubApp;
+
+import java.util.Optional;
+
+import javax.annotation.Nullable;
 
 import com.merkle.oss.magnolia.definition.builder.action.AbstractActionDefinitionBuilder;
 
 public class OpenDetailSubappActionDefinitionBuilder extends AbstractActionDefinitionBuilder<OpenDetailSubappActionDefinition, OpenDetailSubappActionDefinitionBuilder> {
+    @Nullable
+    private ViewType viewType;
 
     public OpenDetailSubappActionDefinitionBuilder() {}
     public OpenDetailSubappActionDefinitionBuilder(final OpenDetailSubappActionDefinition definition) {
         super(definition);
+    }
+
+    public OpenDetailSubappActionDefinitionBuilder viewType(final ViewType viewType) {
+        this.viewType = viewType;
+        return self();
     }
 
     public OpenDetailSubappActionDefinition build(final String name,  final String appName, final String subAppName) {
@@ -16,6 +28,21 @@ public class OpenDetailSubappActionDefinitionBuilder extends AbstractActionDefin
         populate(definition, name);
         definition.setAppName(appName);
         definition.setSubAppName(subAppName);
+        Optional.ofNullable(viewType).map(ViewType::getViewType).ifPresent(definition::setViewType);
         return definition;
+    }
+
+    public enum ViewType {
+        ADD(ContentDetailSubApp.VIEW_TYPE_ADD),
+        EDIT(ContentDetailSubApp.VIEW_TYPE_EDIT),
+        VIEW(ContentDetailSubApp.VIEW_TYPE_VIEW),
+        VERSION(ContentDetailSubApp.VIEW_TYPE_VERSION);
+        private final String viewType;
+        ViewType(final String viewType) {
+            this.viewType = viewType;
+        }
+        private String getViewType() {
+            return viewType;
+        }
     }
 }
