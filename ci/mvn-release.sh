ea643d38b5295c46e37f5d3814d1637b9abdab02
Michael Rauch <michael.rauch@merkle.com>
2025-10-15 07:59:30 +0200

    change CI/CD pipelines to allow multiple Magnolia versions
    # Conflicts:
    #	.github/workflows/release-and-deploy-release-6_2.yml
    #	.github/workflows/release-and-deploy-release.yml
    #	.github/workflows/verify-6_2.yml

--- ci/mvn-release.sh
+++ ci/mvn-release.sh
@@ -3,10 +3,22 @@
 CURRENT_VERSION=`xmllint --xpath '/*[local-name()="project"]/*[local-name()="version"]/text()' pom.xml`
 
 if [[ $CURRENT_VERSION == *-SNAPSHOT ]]; then
+  CURRENT_BRANCH=`git branch --show-current`
+
 	NEW_VERSION=${CURRENT_VERSION%'-SNAPSHOT'}
 	NEXT_VERSION=`bash ci/semver.sh -p $NEW_VERSION`
-	NEXT_SNAPSHOT="$NEXT_VERSION-SNAPSHOT"
-	echo "perform release of $NEW_VERSION from $CURRENT_VERSION and set next develop version $NEXT_SNAPSHOT"
+	MAGNOLIA_VERSION=`echo $CURRENT_BRANCH | cut -f2 -d '-'`
+
+	if [[ $MAGNOLIA_VERSION =~ ^[0-9]+_[0-9]+$ ]]; then
+      NEXT_SNAPSHOT_VERSION="$NEXT_VERSION-M$MAGNOLIA_VERSION-SNAPSHOT"
+      DEVELOP_BRANCH='develop-'$MAGNOLIA_VERSION
+    else
+      NEXT_SNAPSHOT_VERSION="$NEXT_VERSION-SNAPSHOT"
+      DEVELOP_BRANCH='develop'
+    fi
+
+	echo "perform release of $NEW_VERSION from $CURRENT_VERSION and set next develop version $NEXT_SNAPSHOT_VERSION"
+	echo "using current branch: $CURRENT_BRANCH and develop branch: $DEVELOP_BRANCH"
 
 	mvn versions:set -DnewVersion=$NEW_VERSION versions:commit --no-transfer-progress
 
@@ -20,15 +32,15 @@ if [[ $CURRENT_VERSION == *-SNAPSHOT ]]; then
 	echo "create tag for new release"
 	git tag -a $NEW_VERSION -m "Release $NEW_VERSION: tag release"
 
-	echo "merge main back to develop"
+	echo "merge $CURRENT_BRANCH back to $DEVELOP_BRANCH"
 	git fetch --all
-	git checkout develop
-	git merge main
+	git checkout $DEVELOP_BRANCH
+	git merge $CURRENT_BRANCH
 
-	mvn versions:set -DnewVersion=$NEXT_SNAPSHOT versions:commit --no-transfer-progress
+	mvn versions:set -DnewVersion=$NEXT_SNAPSHOT_VERSION versions:commit --no-transfer-progress
 
 	echo "commit new snapshot version"
-	git commit -a -m "Release $NEW_VERSION: set develop to next development version $NEXT_SNAPSHOT"
+	git commit -a -m "Release $NEW_VERSION: set develop to next development version $NEXT_SNAPSHOT_VERSION"
 
 	git push --all
 	git push --tags
