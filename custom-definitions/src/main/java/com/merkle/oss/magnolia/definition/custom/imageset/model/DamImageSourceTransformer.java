39272ff3e6b70fd5c628e0fa144c9e37c54e76bc
eschleb <bjoern.eschle@merkle.com>
2025-12-17 08:58:33 +0100

    Fix alt text not localized

--- custom-definitions/src/main/java/com/merkle/oss/magnolia/definition/custom/imageset/model/DamImageSourceTransformer.java
+++ custom-definitions/src/main/java/com/merkle/oss/magnolia/definition/custom/imageset/model/DamImageSourceTransformer.java
@@ -4,18 +4,31 @@ import com.merkle.oss.magnolia.definition.custom.imageset.ImageType;
 import com.merkle.oss.magnolia.definition.custom.imageset.ImageTypes;
 import com.merkle.oss.magnolia.definition.custom.imageset.model.ImageModel.ImageSource;
 import com.merkle.oss.magnolia.definition.custom.imageset.model.ImageModel.ImageSourceTransformer;
+import com.merkle.oss.magnolia.powernode.PowerNodeService;
+import com.merkle.oss.magnolia.powernode.ValueConverter;
+
+import info.magnolia.dam.api.Asset;
+import info.magnolia.dam.jcr.AbstractJcrItem;
+import info.magnolia.dam.jcr.AssetNodeTypes;
 import info.magnolia.dam.templating.functions.DamTemplatingFunctions;
 
 import jakarta.inject.Inject;
 import java.util.Locale;
 import java.util.Optional;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class DamImageSourceTransformer implements ImageSourceTransformer {
-	private final DamTemplatingFunctions damTemplatingFunctions;
+    private final PowerNodeService powerNodeService;
+    private final DamTemplatingFunctions damTemplatingFunctions;
 
 	@Inject
-	public DamImageSourceTransformer(final DamTemplatingFunctions damTemplatingFunctions) {
-		this.damTemplatingFunctions = damTemplatingFunctions;
+	public DamImageSourceTransformer(
+			final PowerNodeService powerNodeService,
+			final DamTemplatingFunctions damTemplatingFunctions
+	) {
+        this.powerNodeService = powerNodeService;
+        this.damTemplatingFunctions = damTemplatingFunctions;
 	}
 
 	@Override
@@ -28,7 +41,25 @@ public class DamImageSourceTransformer implements ImageSourceTransformer {
 		return Optional
 				.ofNullable(damTemplatingFunctions.getAsset(assetId))
 				.map(asset ->
-					new ImageSource(asset.getLink(), asset.getCaption())
+					new ImageSource(asset.getLink(), getAltText(locale, asset))
+				);
+	}
+
+	protected String getAltText(final Locale locale, final Asset asset) {
+		return getLocalizedStringFromNode(asset, locale, AssetNodeTypes.Asset.CAPTION).or(() ->
+				Optional.of(asset.getCaption()).filter(StringUtils::isNotEmpty)
+		).orElseGet(asset::getName);
+	}
+
+	private Optional<String> getLocalizedStringFromNode(final Asset asset, final Locale locale, final String key) {
+		return Optional
+				.of(asset)
+				.filter(AbstractJcrItem.class::isInstance)
+				.map(AbstractJcrItem.class::cast)
+				.map(AbstractJcrItem::getNode)
+				.map(powerNodeService::convertToPowerNode)
+				.flatMap(assetNode ->
+						assetNode.getProperty(key, locale, ValueConverter::getString)
 				);
 	}
 }
--- custom-definitions/src/main/java/com/merkle/oss/magnolia/definition/custom/videoset/model/DamVideoSourceTransformer.java
+++ custom-definitions/src/main/java/com/merkle/oss/magnolia/definition/custom/videoset/model/DamVideoSourceTransformer.java
@@ -4,18 +4,31 @@ import com.merkle.oss.magnolia.definition.custom.videoset.VideoType;
 import com.merkle.oss.magnolia.definition.custom.videoset.VideoTypes;
 import com.merkle.oss.magnolia.definition.custom.videoset.model.VideoModel.VideoSource;
 import com.merkle.oss.magnolia.definition.custom.videoset.model.VideoModel.VideoSourceTransformer;
+import com.merkle.oss.magnolia.powernode.PowerNodeService;
+import com.merkle.oss.magnolia.powernode.ValueConverter;
+
+import info.magnolia.dam.api.Asset;
+import info.magnolia.dam.jcr.AbstractJcrItem;
+import info.magnolia.dam.jcr.AssetNodeTypes;
 import info.magnolia.dam.templating.functions.DamTemplatingFunctions;
 
 import jakarta.inject.Inject;
 import java.util.Locale;
 import java.util.Optional;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class DamVideoSourceTransformer implements VideoSourceTransformer {
-	private final DamTemplatingFunctions damTemplatingFunctions;
+    private final PowerNodeService powerNodeService;
+    private final DamTemplatingFunctions damTemplatingFunctions;
 
 	@Inject
-	public DamVideoSourceTransformer(final DamTemplatingFunctions damTemplatingFunctions) {
-		this.damTemplatingFunctions = damTemplatingFunctions;
+	public DamVideoSourceTransformer(
+			final PowerNodeService powerNodeService,
+			final DamTemplatingFunctions damTemplatingFunctions
+	) {
+        this.powerNodeService = powerNodeService;
+        this.damTemplatingFunctions = damTemplatingFunctions;
 	}
 
 	@Override
@@ -26,7 +39,25 @@ public class DamVideoSourceTransformer implements VideoSourceTransformer {
 	@Override
 	public Optional<VideoSource> transform(final Locale locale, final String src) {
 		return Optional.ofNullable(damTemplatingFunctions.getAsset(src)).map(asset ->
-			new VideoSource(asset.getLink(), asset.getCaption())
+			new VideoSource(asset.getLink(), getAltText(locale, asset))
 		);
+	}
+
+	protected String getAltText(final Locale locale, final Asset asset) {
+		return getLocalizedStringFromNode(asset, locale, AssetNodeTypes.Asset.CAPTION).or(() ->
+				Optional.of(asset.getCaption()).filter(StringUtils::isNotEmpty)
+		).orElseGet(asset::getName);
+	}
+
+	private Optional<String> getLocalizedStringFromNode(final Asset asset, final Locale locale, final String key) {
+		return Optional
+				.of(asset)
+				.filter(AbstractJcrItem.class::isInstance)
+				.map(AbstractJcrItem.class::cast)
+				.map(AbstractJcrItem::getNode)
+				.map(powerNodeService::convertToPowerNode)
+				.flatMap(assetNode ->
+						assetNode.getProperty(key, locale, ValueConverter::getString)
+				);
 	}
 }
